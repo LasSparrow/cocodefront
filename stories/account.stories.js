95590ef36b1a773c896c5c81926b1945a71f2c0e
Megan Holmes <lassparrow@Megans-MacBook-Pro.local>
2020-11-02 13:42:49 -0800

    added tutor comp

--- stories/account.stories.js
+++ stories/account.stories.js
@@ -4,10 +4,11 @@ import EditProfPic from '../comps/EditProfPic';
 import ListLine from '../comps/ListLine';
 import List from '../comps/List';
 import AccountEdit from '../comps/AccountEdit';
+import TutorComp from '../comps/TutorComp';
 
 export default {
     title: 'Account', 
-    component: EditItem, EditProfPic, ListLine, List, AccountEdit
+    component: EditItem, EditProfPic, ListLine, List, AccountEdit, TutorComp
 }; 
 
 export const EditItemComp = () => <EditItem />;
@@ -16,3 +17,4 @@ export const ProfPicComp = () => <EditProfPic picture="/guy.jpg" />;
 export const ListLineComp = () => <ListLine />;
 export const ListComp = () => <List />;
 export const AccountEditComp = () => <AccountEdit />;
+export const TutorCompComp = () => <TutorComp />;
