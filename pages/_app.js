c9c8ecd66bb92deae255d77d13a75eb23b4872bd
= <=>
2020-11-04 10:14:36 -0800

    made some changes to pages

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import './app.scss';
-import React from 'react'; 
-import Sidebar from '../comps/Sidebar'; 
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}></Component>
-}
-
-export default MyApp;
