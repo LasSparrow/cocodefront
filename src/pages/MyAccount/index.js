8d8900219f88a089248b2790dfe59af2f88cd565
Megan Holmes <lassparrow@Megans-MacBook-Pro.local>
2020-11-04 18:49:08 -0800

    readded public

--- src/pages/MyAccount/index.js
+++ src/pages/MyAccount/index.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react';
+import Sidebar from '../../comps/Sidebar';
+import EditProfPic from '../../comps/EditProfPic';
+import AccountEdit from '../../comps/AccountEdit';
+
 export default function MyAccount() {
   const HandleBoxClick = (str)=>{
     alert(str);
   }
 
   return <div className="home">
-
+    <Sidebar />
+    <EditProfPic />
+    <AccountEdit />
   </div>
 }
