727698375face1b0175e0281e75f11725e933d1d
parsasi <parsasi@rocketmail.com>
2020-11-13 02:37:31 +0330

    api folder created to host api call functions authTest function checks the given token with server to determine is validity serverUrl helper holds the base url of the server for all the api calls

---
+++ src/api/auth/authTest.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import serverUrl from '../../helpers/serverUrl'
+import path from 'path'
+
+const authTest =  async (token) => {
+    const endpoint = '/auth/test'
+
+    const config = {
+        headers: {
+          'Authorization': token
+        }
+    }
+
+    const url = path.join(serverUrl , endpoint)
+
+    return await (await axios.get(url , config)).data
+}
+
+export default authTest
+
+
+// const [token , ] = useToken()
+
+
+
+//   useEffect(() => {
+
+//     async function fetch(){
+//       return await authTest(token)
+//     }
+
+//     fetch()
+//   } , [token])
