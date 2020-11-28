235b49f27e93199277b48fcc7dcd2f61d3cffd8d
parsasi <parsasi@rocketmail.com>
2020-11-28 21:39:03 +0330

    Loading component added Loading component rendered on Explore and US(UpcomingSession) components while loading state is true

--- src/comps/ExploreCategories/index.js
+++ src/comps/ExploreCategories/index.js
@@ -1,14 +1,17 @@
 import React , {useEffect , useState} from 'react'
 import ExploreCategory from '../ExploreCategory'
 import getCategories from '../../api/category/getCategories'
+import Loading from '../Loading/index'
 
 export default function ExploreCategories(props){
     const [categories , setCategories] = useState([])
+    const [loading , setLoading] = useState(true)
 
     useEffect(() => {
         async function fetch(){
             const data = await getCategories()
             setCategories(data)
+            setLoading(false)
         }
         fetch()
     },[])
@@ -16,13 +19,10 @@ export default function ExploreCategories(props){
     return (
        <>
         {
-            categories.map(item => <ExploreCategory key={item.id} text={item.text} image={item.photo}/>)
+            !loading ?
+                categories.map(item => <ExploreCategory key={item.id} text={item.text} image={item.photo}/>)
+                : <Loading />
         }
        </>
     )
 }
-
-        //  ExploreCategory text="html" tutors="12" students="34" image={'/WebDevImage.png'}
-        // <ExploreCategory text="JavaScript" tutors="8" students="13" image={'/JavaScriptImage.png'} />
-        // <ExploreCategory text="CSS" tutors="17" students="41" image={'/CSSImage.png'} /> 
-        // <ExploreCategory  text="Python" tutors="4" students="2" image={'/PythonImage.jpg'} />
